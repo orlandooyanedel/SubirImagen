70d31ebe696244f24a1fbef231f700cedc1449e1
cetecom <cetecom@LCV10419>
2019-04-26 16:57:42 -0300

    Subir y guardar imagen

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+        <form nameform="perfil" action="procesaenvio.php" id="perfil" method="POST" enctype="multipart/form-data">
+                <input type="file" name="imagen" id="imagen">
+                <input type="submit" value="subir">
+            </form>
+    </body>
+</html>
---
+++ procesaenvio.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$ubicacion="C:/xampp/htdocs/Ejercicio_26_04/img/".$_FILES["imagen"]["name"];
+move_uploaded_file($_FILES["imagen"]["tmp_name"], $ubicacion);
